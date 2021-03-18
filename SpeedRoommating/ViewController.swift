b2cfbb0b13d302bfa2943389fd1065797dacf95d
Thomas Lee <thomas.lee999@gmail.com>
2021-03-18 19:51:36 +0000

    Initial Commit

---
+++ SpeedRoommating/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeedRoommating
+//
+//  Created by Thomas Lee on 18/03/2021.
+//  Copyright © 2021 Thomas Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
